4c0d98348b0cbba785272fc72e6029632f8337d8
Mikhail B <wproxym@at.gmail>
2022-01-28 11:24:15 +0300

    simplify

--- deps/js/ahbr.js
+++ deps/js/ahbr.js
@@ -10,73 +10,81 @@ function andThenMakeSmartLink (oldMarker, newMarker, confirmAction, afterAction)
 		//logger.debug(datas);
 		
 		var requestType = datas.requestType;
-		if (requestType !== 'form') {
-			logger.error('only form request supported here');
-			return false;
-		}
-		
-		var ar = (''+e.href).split('#');
-		var dataAction = ar[0]; // href
-		t.attr('href', dataAction+'#'+newMarker);
-		
-		var frname = 'dummyframe';
-		var fr = $('iframe#'+frname)[0] || $('iframe[name="'+frname+'"]')[0];
-		if (!fr) {
-			fr = $('<iframe name="'+frname+'" id="'+frname+'" style="display: none;"></iframe>');
-			$('body').append(fr);
-		}
-		frname = $(fr).attr('name');
-		
-		var $form = $("<form></form>").attr({ method: datas.method || "get", action: dataAction, target: frname}).css("display","none");
-		
-		if (datas.data) {
-			try {
-				if (datas.data.length || datas.data.length === '') {
-					datas.data = decodeURIComponent(datas.data);
-					datas.data = Object.fromEntries(new URLSearchParams(datas.data));
+		switch (requestType) {
+			case 'form':
+				var ar = (''+e.href).split('#');
+				var dataAction = ar[0]; // href
+				t.attr('href', dataAction+'#'+newMarker);
+				
+				var frname = 'dummyframe';
+				var fr = $('iframe#'+frname)[0] || $('iframe[name="'+frname+'"]')[0];
+				if (!fr) {
+					fr = $('<iframe name="'+frname+'" id="'+frname+'" style="display: none;"></iframe>');
+					$('body').append(fr);
 				}
-			} catch (e1) {
-			}
-			try {
-				if (!datas.data.length) {
-					datas.data = JSON.stringify(datas.data);
+				frname = $(fr).attr('name');
+				
+				var $form = $("<form></form>").attr({ method: datas.method || "get", action: dataAction, target: frname}).css("display","none");
+				
+				if (datas.data) {
+					try {
+						if (datas.data.length || datas.data.length === '') {
+							datas.data = decodeURIComponent(datas.data);
+							datas.data = Object.fromEntries(new URLSearchParams(datas.data));
+						}
+					} catch (e1) {
+					}
+					try {
+						if (!datas.data.length) {
+							datas.data = JSON.stringify(datas.data);
+						}
+					} catch (e2) {
+					}
+					try {
+						datas.data = JSON.parse(datas.data);
+					} catch (e3) {
+						datas.data = "Error";
+						return false;
+					}											
+				
+					for (var key in datas.data) {
+						$form.append($("<input/>").attr({type: "hidden", name: key, value: datas.data[key]}));
+					}
 				}
-			} catch (e2) {
-			}
-			try {
-				datas.data = JSON.parse(datas.data);
-			} catch (e3) {
-				datas.data = "Error";
+				$('body').append($form);
+				t.data("onclick", e.onclick);
+				e.onclick = 'return false;';
+				t.data("postform",$form);
+				
+				t.click(function (ev) {
+					ev.preventDefault();
+					if ($(this).data("postform") && (!(confirmAction && (typeof confirmAction) === 'function') || confirmAction())) {
+						$(this).data("postform").submit();
+					} else {
+						logger.warn("No .postform or canceled");
+						return false;
+					}
+					var f = $(this).data("onclick") || function () {};
+					$(this).data("onclick", "");
+					$(this).css('pointer-events', 'none');
+					$(this).css('cursor', 'default');
+					setTimeout(function () {
+						f();
+						if (afterAction && (typeof afterAction) === 'function') {
+							afterAction();
+						}
+					}.bind(this), 500);
+					return true;
+				});
+				
+				break;
+			case 'ajax':
+				alert('unimplemented!');
+				break;
+			default:
+				logger.error('only form request supported here');
 				return false;
-			}											
-		
-			for (var key in datas.data) {
-				$form.append($("<input/>").attr({type: "hidden", name: key, value: datas.data[key]}));
-			}
-		}
-		$('body').append($form);
-		t.data("onclick", e.onclick);
-		e.onclick = 'return false;';
-		t.data("postform",$form);											
-	}).click(function (ev) {
-		ev.preventDefault();
-		if ($(this).data("postform") && (!(confirmAction && (typeof confirmAction) === 'function') || confirmAction())) {
-			$(this).data("postform").submit();
-		} else {
-			logger.warn("No .postform or canceled");
-			return false;
-		}
-		var f = $(this).data("onclick") || function () {};
-		$(this).data("onclick", "");
-		$(this).css('pointer-events', 'none');
-		$(this).css('cursor', 'default');
-		setTimeout(function () {
-			f();
-			if (afterAction && (typeof afterAction) === 'function') {
-				afterAction();
-			}
-		}.bind(this), 500);
-		return true;
+		}											
 	});
 	return true;
 };
